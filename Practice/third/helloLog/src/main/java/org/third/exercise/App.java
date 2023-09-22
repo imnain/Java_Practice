9752a4d1bf383943ff126d8224075d956de3a83c
mandy <mandeepnain84@gmail.com>
2023-09-22 22:17:00 +0530

    log4j2 replaced with logback

--- Practice/third/helloLog/src/main/java/org/third/exercise/App.java
+++ Practice/third/helloLog/src/main/java/org/third/exercise/App.java
@@ -1,7 +1,10 @@
 package org.third.exercise;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+//import org.apache.logging.log4j.LogManager;
+//import org.apache.logging.log4j.Logger;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Hello world!
@@ -9,7 +12,8 @@ import org.apache.logging.log4j.Logger;
  */
 public class App 
 {	
-    private static final Logger logger = LogManager.getLogger(App.class);
+   // private static final Logger logger = LogManager.getLogger(App.class);
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
     public static void main( String[] args )
     {	
     	logger.info("Hello World from logger");
